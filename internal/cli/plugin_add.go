dbcd220115537cf23c0260a275d71c8ca91d5595
Aaron Klinker <aaronklinker1@gmail.com>
2021-01-26 08:57:19 -0600

    feat(cli): Add and remove plugins

--- internal/cli/plugin_add.go
+++ internal/cli/plugin_add.go
@@ -9,29 +9,29 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var pluginRemoveCmd = &cobra.Command{
-	Use:   "plugins:remove",
-	Short: "Stop and remove an installed plugin",
-	Long: `Stop and remove an installed plugins: Traefik
+var pluginInstallCmd = &cobra.Command{
+	Use:   "plugins:add",
+	Short: "Install and start a pre-defined plugin",
+	Long: `Install one of the pre-defined plugins: Traefik
 	
 Plugins are simple, pre-configured, reusable applications. Every plugin can also be defined as an
 app.`,
 	Args:      cobra.ExactValidArgs(1),
 	ValidArgs: []string{"traefik"},
 	Run: func(cmd *cobra.Command, args []string) {
-		removePlugin(args[0])
+		installPlugin(args[0])
 	},
 }
 
 func init() {
-	RootCmd.AddCommand(pluginRemoveCmd)
+	RootCmd.AddCommand(pluginInstallCmd)
 }
 
-func removePlugin(pluginName string) {
-	fmt.Printf("Removing %s...\n", pluginName)
-	uninstallParams := operations.NewUninstallPluginParams()
-	uninstallParams.PluginName = pluginName
-	_, err := config.Client().Operations.UninstallPlugin(uninstallParams)
+func installPlugin(pluginName string) {
+	fmt.Printf("Adding %s...\n", pluginName)
+	installParams := operations.NewInstallPluginParams()
+	installParams.PluginName = pluginName
+	_, err := config.Client().Operations.InstallPlugin(installParams)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
