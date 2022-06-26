232a4380665396dad49672d99a1cc0be5d9d8a8a
Aaron Klinker <aaronklinker1@gmail.com>
2022-06-26 12:01:41 -0500

    ci: fix publish server action

---
+++ .github/actions/filter-changelog/index.js
@@ -0,0 +1,37 @@
+const core = require("@actions/core");
+
+try {
+  // Read inputs
+  const input = core.getInput("changelog");
+  const inputLines = input.split("\n");
+  const scopes = core
+    .getInput("scopes")
+    .split(",")
+    .map((s) => s.trim());
+
+  // Filter Changelog
+  const outputLines = [];
+  let scopedCount = 0;
+  inputLines.forEach((line) => {
+    // Line isn't a bullet point
+    if (!line.startsWith("* ")) return outputLines.push(line);
+
+    // Bullet point starts with the scope prefix
+    const matchesScope = !!scopes.find((scope) =>
+      line.toLowerCase().startsWith(`* **${scope.toLowerCase()}:**`)
+    );
+    if (matchesScope) {
+      scopedCount++;
+      return outputLines.push(line);
+    }
+  });
+  const output = outputLines.join("\n");
+  const empty = scopedCount === 0;
+
+  // Set outputs
+  console.log({ input, output, empty });
+  core.setOutput("changelog", output);
+  core.setOutput("empty", empty);
+} catch (error) {
+  core.setFailed(error.message);
+}
