1ce5a36bf13a9c7d45f5881e9829748aa2ad0fac
Aaron Klinker <aaronklinker1@gmail.com>
2022-07-06 18:59:59 -0500

    fix(ui): helpful details for main app list
    Add loading indicator, error message, and empty message

--- web/src/components/AppList.vue
+++ web/src/components/AppList.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { computed } from "vue";
-import { useAppListQuery } from "../composition/list-apps-query";
+import { App, useAppListQuery } from "../composition/list-apps-query";
 
 const props = defineProps<{
   showHidden?: boolean;
@@ -8,11 +8,14 @@ const props = defineProps<{
 const showHidden = computed(() => props.showHidden);
 const clusterIpAddress = location.hostname;
 
-const { result } = useAppListQuery({ showHidden, clusterIpAddress });
+const { result, loading, error } = useAppListQuery({
+  showHidden,
+  clusterIpAddress,
+});
 
-const groups = computed(() => {
+const groups = computed<Array<[string, App[]]>>(() => {
   const apps = result.value?.apps;
-  if (apps == null) return null;
+  if (apps == null || apps.length === 0) return [["", []]];
 
   return Object.entries<any[]>(
     apps.reduce((grouped: Record<string, any>, app: any) => {
@@ -26,8 +29,19 @@ const groups = computed(() => {
 </script>
 
 <template>
-  <div v-if="result && result.apps" class="space-y-8">
-    <div class="w-full shadow-2xl" v-for="[group, apps] of groups" :key="group">
+  <!-- Loading -->
+  <div v-if="loading && result == null" class="loading m-8" />
+
+  <!-- Error -->
+  <p v-else-if="error" class="text-error">{{ error }}</p>
+
+  <!-- Apps Table -->
+  <div v-if="result" class="space-y-8">
+    <div
+      class="w-full shadow-2xl rounded-lg"
+      v-for="[group, apps] of groups"
+      :key="group"
+    >
       <table class="table w-full">
         <thead>
           <tr>
@@ -38,10 +52,14 @@ const groups = computed(() => {
           </tr>
         </thead>
 
+        <!-- Main list -->
         <tbody>
           <app-list-item v-for="app of apps" :key="app.id" :app="app" />
         </tbody>
       </table>
+
+      <!-- Empty message -->
+      <p v-if="apps.length === 0" class="text-center w-full p-6">No apps</p>
     </div>
   </div>
 </template>
